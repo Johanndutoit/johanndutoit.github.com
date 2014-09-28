1687bd0c6c6bc725166b1410916f206da3f84c5b
Johann du Toit <exxonno@gmail.com>
2014-09-28 18:50:15 +0200

    Updated with new presentations

---
+++ presentations/2014/09/resolving-your-intimacy-issues-with-nodejs-29-sept-2014/slide_config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Resolving your intimacy problems with NodeJS',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/js_logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Johann du Toit',
+    company: '',
+    gplus: 'google.com/+JohannDuToit',
+    twitter: '',
+    www: 'johanndutoit.net',
+    github: 'github.com/johanndutoit'
+  }]
+};
+
